7c848a71550510bb3a3f8c2c6b16315be843d55c
j0ck-de <josef.molz1985@gmail.com>
2020-06-28 20:40:46 +0200

    feature: added netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
