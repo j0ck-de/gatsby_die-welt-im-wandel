a38e472f101ca0aff3664d6b371317eb651f0389
j0ck-de <josef.molz1985@gmail.com>
2020-06-28 20:17:17 +0200

    feature: create pages when md file is create + added template for articles

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,48 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = function ({ data }) {
+  console.log(data);
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link to={node.fields.slug}>
+            <h3>
+              {node.frontmatter.title}{" "}
+              <span>
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+          </Link>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </Layout>
+  );
+}
 
-export default IndexPage
+export default IndexPage;
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
