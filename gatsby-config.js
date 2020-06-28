7f792bb085ee25135af440690a0ba01883bc2762
j0ck-de <josef.molz1985@gmail.com>
2020-06-28 21:07:51 +0200

    fix: changed folder structure and fixed path

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `articles`,
-        path: `${__dirname}/src/articles/`,
+        path: `${__dirname}/content/articles/`,
       },
     },
     `gatsby-transformer-remark`,
